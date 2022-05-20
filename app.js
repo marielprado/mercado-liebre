574b42334992f452b5c4fe196b86fa290d6fc99d
marielprado <mariel.prado24@gmail.com>
2022-05-19 21:17:20 -0300

    fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-const PORT = 3030
+const PORT = process.env.PORT || 3030
 app.listen(PORT, ()=>
     {console.log("Servidor corriendo en puerto " + PORT )}
     )
